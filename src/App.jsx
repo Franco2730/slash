ddcd4984e4fde74faac837fcfe71b2a3851e7237
franco2730 <franco.rosales2130@gmail.com>
2024-06-10 22:26:56 -0300

    Slash

--- src/App.jsx
+++ src/App.jsx
@@ -1,348 +1,110 @@
+import { useState } from "react";
+import Guitarr from "./components/Guitarr";
 import Header from "./components/Header";
 
 // Los componentes SIEMPRE deben iniciar con mayusculas. "App"
 
 function App() {
+
+  // Los estados o hooks se colocan antes del return y NO pueden ir dentro de funciones ni condicionales.
+  const [auth, setAuth] = useState(true)
+  
+  console.log(auth);
+  
+
+
   // El return es lo que muestra el componente en pantalla.
   return (
     <>
-      <Header/>
-      <header class="py-5 header">
-        <div class="container-xl">
-          <div class="row justify-content-center justify-content-md-between">
-            <div class="col-8 col-md-3">
-              <a href="index.html">
-                <img
-                  class="img-fluid"
-                  src="./public/img/logo.svg"
-                  alt="imagen logo"
-                />
-              </a>
-            </div>
-            <nav class="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-              <div class="carrito">
-                <img
-                  class="img-fluid"
-                  src="./public/img/carrito.png"
-                  alt="imagen carrito"
-                />
+      <Header />
 
-                <div id="carrito" class="bg-white p-3">
-                  <p class="text-center">El carrito esta vacio</p>
-                  <table class="w-100 table">
-                    <thead>
-                      <tr>
-                        <th>Imagen</th>
-                        <th>Nombre</th>
-                        <th>Precio</th>
-                        <th>Cantidad</th>
-                        <th></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>
-                          <img
-                            class="img-fluid"
-                            src="./public/img/guitarra_02.jpg"
-                            alt="imagen guitarra"
-                          />
-                        </td>
-                        <td>SRV</td>
-                        <td class="fw-bold">$299</td>
-                        <td class="flex align-items-start gap-4">
-                          <button type="button" class="btn btn-dark">
-                            -
-                          </button>
-                          1
-                          <button type="button" class="btn btn-dark">
-                            +
-                          </button>
-                        </td>
-                        <td>
-                          <button class="btn btn-danger" type="button">
-                            X
-                          </button>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
 
-                  <p class="text-end">
-                    Total pagar: <span class="fw-bold">$899</span>
-                  </p>
-                  <button class="btn btn-dark w-100 mt-3 p-2">
-                    Vaciar Carrito
-                  </button>
-                </div>
-              </div>
-            </nav>
-          </div>
-        </div>
-      </header>
+        <div className="row mt-5">
+          <Guitarr />
 
-      <main class="container-xl mt-5">
-        <h2 class="text-center">Nuestra Colección</h2>
-
-        <div class="row mt-5">
-          <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-            <div class="col-4">
-              <img
-                class="img-fluid"
-                src="./public/img/guitarra_01.jpg"
-                alt="imagen guitarra"
-              />
-            </div>
-            <div class="col-8">
-              <h3 class="text-black fs-4 fw-bold text-uppercase">Lukather</h3>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
-                quae labore odit magnam in autem nesciunt, amet deserunt
-              </p>
-              <p class="fw-black text-primary fs-3">$299</p>
-              <button type="button" class="btn btn-dark w-100">
-                Agregar al Carrito
-              </button>
-            </div>
-          </div>
-
-          <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-            <div class="col-4">
-              <img
-                class="img-fluid"
-                src="./public/img/guitarra_02.jpg"
-                alt="imagen guitarra"
-              />
-            </div>
-            <div class="col-8">
-              <h3 class="text-black fs-4 fw-bold text-uppercase">SRV</h3>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
-                quae labore odit magnam in autem nesciunt, amet deserunt
-              </p>
-              <p class="fw-black text-primary fs-3">$299</p>
-              <button type="button" class="btn btn-dark w-100 ">
-                Agregar al Carrito
-              </button>
-            </div>
-          </div>
-
-          <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-            <div class="col-4">
-              <img
-                class="img-fluid"
-                src="./public/img/guitarra_03.jpg"
-                alt="imagen guitarra"
-              />
-            </div>
-            <div class="col-8">
-              <h3 class="text-black fs-4 fw-bold text-uppercase">Borland</h3>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
-                quae labore odit magnam in autem nesciunt, amet deserunt
-              </p>
-              <p class="fw-black text-primary fs-3">$299</p>
-              <button type="button" class="btn btn-dark w-100 ">
-                Agregar al Carrito
-              </button>
-            </div>
-          </div>
-
-          <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-            <div class="col-4">
-              <img
-                class="img-fluid"
-                src="./public/img/guitarra_04.jpg"
-                alt="imagen guitarra"
-              />
-            </div>
-            <div class="col-8">
-              <h3 class="text-black fs-4 fw-bold text-uppercase">Vai</h3>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
-                quae labore odit magnam in autem nesciunt, amet deserunt
-              </p>
-              <p class="fw-black text-primary fs-3">$299</p>
-              <button type="button" class="btn btn-dark w-100 ">
-                Agregar al Carrito
-              </button>
-            </div>
-          </div>
-
-          <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-            <div class="col-4">
-              <img
-                class="img-fluid"
-                src="./public/img/guitarra_05.jpg"
-                alt="imagen guitarra"
-              />
-            </div>
-            <div class="col-8">
-              <h3 class="text-black fs-4 fw-bold text-uppercase">Thompson</h3>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
-                quae labore odit magnam in autem nesciunt, amet deserunt
-              </p>
-              <p class="fw-black text-primary fs-3">$299</p>
-              <button type="button" class="btn btn-dark w-100 ">
-                Agregar al Carrito
-              </button>
-            </div>
-          </div>
-
-          <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-            <div class="col-4">
-              <img
-                class="img-fluid"
-                src="./public/img/guitarra_06.jpg"
-                alt="imagen guitarra"
-              />
-            </div>
-            <div class="col-8">
-              <h3 class="text-black fs-4 fw-bold text-uppercase">White</h3>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
-                quae labore odit magnam in autem nesciunt, amet deserunt
-              </p>
-              <p class="fw-black text-primary fs-3">$299</p>
-              <button type="button" class="btn btn-dark w-100 ">
-                Agregar al Carrito
-              </button>
-            </div>
-          </div>
-
-          <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-            <div class="col-4">
-              <img
-                class="img-fluid"
-                src="./public/img/guitarra_07.jpg"
-                alt="imagen guitarra"
-              />
-            </div>
-            <div class="col-8">
-              <h3 class="text-black fs-4 fw-bold text-uppercase">Cobain</h3>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
-                quae labore odit magnam in autem nesciunt, amet deserunt
-              </p>
-              <p class="fw-black text-primary fs-3">$299</p>
-              <button type="button" class="btn btn-dark w-100 ">
-                Agregar al Carrito
-              </button>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-            <div class="col-4">
-              <img
-                class="img-fluid"
-                src="./public/img/guitarra_08.jpg"
-                alt="imagen guitarra"
-              />
-            </div>
-            <div class="col-8">
-              <h3 class="text-black fs-4 fw-bold text-uppercase">Dale</h3>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
-                quae labore odit magnam in autem nesciunt, amet deserunt
-              </p>
-              <p class="fw-black text-primary fs-3">$299</p>
-              <button type="button" class="btn btn-dark w-100 ">
-                Agregar al Carrito
-              </button>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-            <div class="col-4">
-              <img
-                class="img-fluid"
-                src="./public/img/guitarra_09.jpg"
-                alt="imagen guitarra"
-              />
-            </div>
-            <div class="col-8">
-              <h3 class="text-black fs-4 fw-bold text-uppercase">Krieger</h3>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
-                quae labore odit magnam in autem nesciunt, amet deserunt
-              </p>
-              <p class="fw-black text-primary fs-3">$299</p>
-              <button type="button" class="btn btn-dark w-100 ">
-                Agregar al Carrito
-              </button>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-            <div class="col-4">
-              <img
-                class="img-fluid"
-                src="./public/img/guitarra_10.jpg"
-                alt="imagen guitarra"
-              />
-            </div>
-            <div class="col-8">
-              <h3 class="text-black fs-4 fw-bold text-uppercase">Campbell</h3>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
-                quae labore odit magnam in autem nesciunt, amet deserunt
-              </p>
-              <p class="fw-black text-primary fs-3">$299</p>
-              <button type="button" class="btn btn-dark w-100 ">
-                Agregar al Carrito
-              </button>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-            <div class="col-4">
-              <img
-                class="img-fluid"
-                src="./public/img/guitarra_11.jpg"
-                alt="imagen guitarra"
-              />
-            </div>
-            <div class="col-8">
-              <h3 class="text-black fs-4 fw-bold text-uppercase">Reed</h3>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
-                quae labore odit magnam in autem nesciunt, amet deserunt
-              </p>
-              <p class="fw-black text-primary fs-3">$299</p>
-              <button type="button" class="btn btn-dark w-100 ">
-                Agregar al Carrito
-              </button>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-            <div class="col-4">
-              <img
-                class="img-fluid"
-                src="./public/img/guitarra_12.jpg"
-                alt="imagen guitarra"
-              />
-            </div>
-            <div class="col-8">
-              <h3 class="text-black fs-4 fw-bold text-uppercase">Hazel</h3>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
-                quae labore odit magnam in autem nesciunt, amet deserunt
-              </p>
-              <p class="fw-black text-primary fs-3">$299</p>
-              <button type="button" class="btn btn-dark w-100 ">
-                Agregar al Carrito
-              </button>
-            </div>
-          </div>
         </div>
       </main>
 
-      <footer class="bg-dark mt-5 py-5">
-        <div class="container-xl">
-          <p class="text-white text-center fs-4 mt-4 m-md-0">
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
             GuitarLA - Todos los derechos Reservados
           </p>
-        </div>
+        </div> 
       </footer>
     </>
   );
 }
 
 export default App;
+
+//* Vamos a detallar los 3 hooks mas importantes y comunes de React:
+
+//* useState - ¿Qué es useState?
+// useState es un hook que te permite añadir estado a componentes funcionales en React. Antes, solo los componentes de clase podían tener estado, pero con useState, los componentes funcionales también pueden gestionar su propio estado.
+
+// ¿Para qué sirve? - useState sirve para manejar datos que pueden cambiar con el tiempo en tu componente. Por ejemplo, un contador, el texto de un input, o cualquier otra información que necesite ser actualizada y reflejada en la interfaz de usuario.
+// Vamos a crear un contador que aumenta cada vez que hacemos clic en un botón:
+
+// import React, { useState } from 'react';
+
+// function Contador() {
+//  Declaramos una variable de estado llamada "contador" y una función para actualizarla "setContador"
+//   const [contador, setContador] = useState(0);
+
+//   return (
+//     <div>
+//       <p>Has hecho clic {contador} veces</p>
+//       <button onClick={() => setContador(contador + 1)}>
+//         Aumentar contador
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default Contador;
+
+//******************************************** */
+
+//* useEffect - ¿Qué es useEffect?
+// useEffect es un hook que permite ejecutar efectos secundarios en componentes funcionales. Los efectos secundarios son cualquier cosa que afecta algo fuera del alcance de la función que se está ejecutando, como una llamada a una API, suscripciones a eventos, o actualizar el DOM.
+
+// ¿Para qué sirve? - useEffect sirve para manejar tareas como:
+
+// Fetching de datos (llamadas a APIs).
+// Suscripciones a eventos.
+// Manejo de intervalos o temporizadores.
+// Limpieza de recursos cuando el componente se desmonta.
+// Vamos a crear un componente que muestra un mensaje en la consola cada vez que el componente se renderiza.
+
+// import React, { useState, useEffect } from 'react';
+
+// function MensajeConsola() {
+//   const [mensaje, setMensaje] = useState('Hola, mundo!');
+
+//   useEffect(() => {
+//     console.log('El componente se ha renderizado o actualizado');
+
+//     return () => {
+//       console.log('El componente se va a desmontar');
+//     };
+//   }, [mensaje]);
+
+//   return (
+//     <div>
+//       <p>{mensaje}</p>
+//       <button onClick={() => setMensaje('¡Hola, React!')}>
+//         Cambiar mensaje
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default MensajeConsola;
+
+
+
+//* Despues podemos encontrar muchos mas hooks
+// useReducer, useCallback, useMemo, useRef y varios más.
